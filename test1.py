5ca9b39f6aaef004c7a3520a91816743b3cc6c95
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-04-23 09:44:47 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,20 @@
+def pirates(d, dp, ep):
+    total_plunder = 0.0
+    for day in range(1, d + 1):
+        total_plunder += dp
+        if day % 3 == 0:
+            total_plunder += dp / 2
+        if day % 5 == 0:
+            total_plunder *= 0.7
+    if total_plunder >= ep:
+        print(f"Ahoy! {total_plunder:.2f} plunder gained.")
+    else:
+        percentage = total_plunder / ep * 100
+        print(f"Collected only {percentage:.2f}% of the plunder.")
+
+
+days = int(input())
+daily_plunder = int(input())
+expected_plunder = float(input())
+
+pirates(days, daily_plunder, expected_plunder)
